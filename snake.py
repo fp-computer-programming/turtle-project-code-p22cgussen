8144923d0a60396b5c10d1074541f984c76b6365
p22cgussen <p22cgussen@fairfieldprep.org>
2022-04-10 20:18:35 -0400

    complete snake game2

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ import random
 import time
 
 
-#creating turtle screen
+# turtle screen
 screen = turtle.Screen()
 screen.title('Snake')
 screen.setup(width = 700, height = 700)
@@ -14,8 +14,7 @@ turtle.bgcolor('purple')
 
 
 
-##creating a border for our game
-
+# border for the game
 turtle.speed(5)
 turtle.pensize(4)
 turtle.penup()
@@ -32,7 +31,7 @@ turtle.forward(500)
 turtle.penup()
 turtle.hideturtle()
 
-#snake
+# snake head turtle
 snake = turtle.Turtle()
 snake.speed(0)
 snake.shape('square')
@@ -41,7 +40,7 @@ snake.penup()
 snake.goto(0,0)
 snake.direction = 'stop'
 
-#food
+# fruit turtle
 fruit = turtle.Turtle()
 fruit.speed(0)
 fruit.shape('circle')
@@ -49,7 +48,7 @@ fruit.color('red')
 fruit.penup()
 fruit.goto(100,0)
 
-body = []
+body = [] # empty list for food after it is eaten
 
 #scoring
 score = 0
@@ -59,7 +58,7 @@ scoring.hideturtle()
 
 
 
-#######define how to move
+# functions for all four directions
 def up():
     if snake.direction != "down":
         snake.direction = "up"
@@ -76,76 +75,61 @@ def right():
     if snake.direction != "left":
         snake.direction = "right"
 
-def move():
+def move(): # function for snake movement
     if snake.direction == "up":
         y = snake.ycor()
         snake.sety(y + 20)
-
     if snake.direction == "down":
         y = snake.ycor()
         snake.sety(y - 20)
-
     if snake.direction == "left":
         x = snake.xcor()
         snake.setx(x - 20)
-
     if snake.direction == "right":
         x = snake.xcor()
         snake.setx(x + 20)
 
-# Keyboard bindings
-screen.listen()
+screen.listen() # binding movement keys to movement functions
 screen.onkeypress(up, "w")
 screen.onkeypress(down, "s")
 screen.onkeypress(left, "a")
 screen.onkeypress(right, "d")
 
-#main loop
 
-while True:
+
+while True: # while loop for actual gameplay
         screen.update()
-            #snake and fruit coliisions
-        if snake.distance(fruit)< 20:
+        if snake.distance(fruit)< 20: # snake eating fruit and keeping score
                 x = random.randint(-290,270)
                 y = random.randint(-240,240)
                 fruit.goto(x,y)
                 scoring.clear()
                 score+=1
                 
-                ## creating new_ball
-                new_fruit = turtle.Turtle()
+                new_fruit = turtle.Turtle() # new fruit spawn
                 new_fruit.speed(0)
                 new_fruit.shape('square')
                 new_fruit.color('red')
                 new_fruit.penup()
                 body.append(new_fruit)
-                
 
-        #adding ball to snake
-        
-        for index in range(len(body)-1,0,-1):
+        for index in range(len(body)-1,0,-1): # add the ball to the snake
                 a = body[index-1].xcor()
                 b = body[index-1].ycor()
-
-                body[index].goto(a,b)
-                                     
+                body[index].goto(a,b)             
         if len(body)>0:
                 a= snake.xcor()
                 b = snake.ycor()
                 body[0].goto(a,b)
-        move()
+        move()  
 
-        ##snake and border collision    
-        if snake.xcor()>280 or snake.xcor()< -300 or snake.ycor()>240 or snake.ycor()<-240:
+        if snake.xcor()>280 or snake.xcor()< -300 or snake.ycor()>240 or snake.ycor()<-240: # snake and border collision
                 time.sleep(1)
                 screen.clear()
                 screen.bgcolor('grey')
                 scoring.goto(0,0)
                 scoring.write("   GAME OVER \n Your Score is {}".format(score),align="center",font=("Arial",40,"bold"))
-
-
-        ## snake collision
-        for food in body:
+        for food in body: # for loop for snake and body collision
                 if food.distance(snake) < 20:
                         time.sleep(1)
                         screen.clear()
@@ -153,8 +137,6 @@ while True:
                         scoring.goto(0,0)
                         scoring.write("   GAME OVER \n Your Score is {}".format(score),align="center",font=("Arial",40,"bold"))
 
-
-                
         time.sleep(delay)
 
 turtle.Terminator()
